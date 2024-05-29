f29eee2b28a39c99baca7acdce22e9070f01e3c4
Ayush Baweja <ayushbaweja@gatech.edu>
2024-05-29 16:11:57 -0400

    parsing statements

---
+++ parse.py
@@ -0,0 +1,120 @@
+import sys
+from lex import *
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.currToken = None
+        self.peekToken = None
+        self.nextToken()
+        self.nextToken() # init current and peek
+
+    # return true if current token matches
+    def checkToken(self, kind):
+        return kind == self.currToken.kind
+
+    # return true if next token matches
+    def checkPeek(self, kind):
+        return kind == self.peekToken.kind
+
+    # try to match current token. if not, error. Advances the the current token
+    def match(self, kind):
+        if not self.checkToken(kind):
+            self.abort("Expected " + kind.name + ", got " + self.currToken.kind.name)
+        self.nextToken()
+
+    # advances the current token
+    def nextToken(self):
+        self.currToken = self.peekToken
+        self.peekToken = self.lexer.getToken()
+
+    def abort(self, message):
+        sys.exit("Error. " + message)
+
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+
+        while self.checkToken(TokenType.NEWLINE):
+            self.nextToken()
+
+        # parsing all
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    def nl(self):
+        print("NEWLINE")
+        self.match(TokenType.NEWLINE) # require at least 1 newline
+        while(self.checkToken(TokenType.NEWLINE)): # allow more
+            self.nextToken()
+    
+    def statement(self):
+        # check first token to identify statement type
+
+        # "PRINT" (expression | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
+            self.nextToken()
+
+            if self.checkToken(TokenType.STRING):
+                self.nextToken()
+            else:
+                self.expression()
+
+        # "IF" comparison "THEN" {statement} "ENDIF"
+        elif self.checkToken(TokenType.IF):
+            print("STATEMENT-IF")
+            self.nextToken()
+            self.comparison()
+
+            self.match(TokenType.THEN)
+            self.nl()
+
+            # zero or more statements
+            while not self.checkToken(TokenType.ENDIF):
+                self.statement()
+            self.match(TokenType.ENDIF)
+        
+        #"WHILE" comparison "REPEAT" {statement} "ENDWHILE"
+        elif self.checkToken(TokenType.WHILE):
+            print("STATEMENT-WHILE")
+            self.nextToken()
+            self.comparison()
+
+            self.match(TokenType.REPEAT)
+            self.nl()
+
+            # zero or more statements
+            while not self.checkToken(TokenType.ENDWHILE):
+                self.statement()
+            self.match(TokenType.ENDWHILE)
+
+        # "LABEL" ident
+        elif self.checkToken(TokenType.LABEL):
+            print("STATEMENT-LABEL")
+            self.nextToken()
+            self.match(TokenType.IDENT)
+        
+        # "GOTO" ident
+        elif self.checkToken(TokenType.GOTO):
+            print("STAEMENT-GOTO")
+            self.nextToken()
+            self.match(TokenType.IDENT)
+        
+        # "LET" ident "=" expression
+        elif self.checkToken(TokenType.LET):
+            print("STAEMENT-LET")
+            self.nextToken()
+            self.match(TokenType.IDENT)
+            self.match(TokenType.EQ)
+            self.expression()
+        
+        # "INPUT" ident
+        elif self.checkToken(TokenType.INPUT):
+            print("STATEMENT-INPUT")
+            self.nextToken()
+            self.match(TokenType.IDENT)
+        
+        else:
+            self.abort("Invalid statement at" + self.currToken.text + "(" + self.currToken.kind.name + ")")
+        self.nl()
